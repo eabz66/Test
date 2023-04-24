a1222b1327628d2d0e5e2d8ed69dc01a91430ce5
eabz66 <ebistrainz@comcast.net>
2023-04-24 11:36:36 -0500

    Update chatgpt_app.py

--- chatgpt_app.py
+++ chatgpt_app.py
@@ -8,7 +8,7 @@ st.title("Creating a front end to ChatGPT using Streamlit - Eduardo Bistrain")
 st.sidebar.image('./jpmchase.png', width=100)
 st.sidebar.header("Instructions")
 st.sidebar.info(
-    '''This is a test web application that serves as a front end to ChatGPT.
+    '''This is a test web application that serves as a front end to ChatGPT.\n
        Enter a **query** in the **text boxes** and **press enter** to receive 
        a **response**.
        '''
